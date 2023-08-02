311e9401d436989ecc6a501ecb487b4f3172ee77
maxence-glt <mgilloteaux05@gmail.com>
2023-08-01 21:38:17 -0400

    Discussion 2 completed

--- Discussion 2.py
+++ Discussion 2.py
@@ -19,3 +19,34 @@ def keep_ints(cond, n):
 
 
 # 1.6
+def make_keeper(n):
+    def num(cond):
+        for x in range(1, n + 1):
+            if cond(x):
+                print(x)
+    return num
+
+
+
+
+
+# 1.1 (Self reference section)
+def print_delayed(x):
+    def delay_print(y): 
+        print(x)
+        return print_delayed(y)
+    return delay_print
+
+
+
+
+
+# 1.2
+def print_n(n):
+    def inner_print(x):
+        if n <= 0:
+            print("done")
+        else:
+            print(x)
+        return print_n(n - 1)
+    return inner_print
