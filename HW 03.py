8ea22dd273ca4682eaf4d0482f000e970c97b704
maxence-glt <mgilloteaux05@gmail.com>
2023-08-01 23:06:15 -0400

    Homework 3 completed

--- HW 03.py
+++ HW 03.py
@@ -30,15 +30,29 @@ def pingpong(n):
     return helper(n, i, 0)
 
 # Q3: Count change
-def count_change(total):
-    index = 1
-    def helper(total, index):
-        if index >= total:
-            return 0
-        if index == 1:
-            return 1 + helper(total, index * 2)
-        else:
-            return (total // index) + helper(total, index * 2)
-    return helper(total, index)
+def count_change(total, cents):
+    if total == 0:
+        return 1
+    elif total < 0:
+        return 0
+    elif cents > total:
+        return 0
+    else: 
+        return count_change(total, cents * 2) + count_change(total - cents, cents)
+
+# Cool way to visualize this that I found (https://www.recursionvisualizer.com/?function_definition=def%20count_change%28total%2C%20cents%29%3A%0A%20%20%20%20if%20total%20%3D%3D%200%3A%0A%20%20%20%20%20%20%20%20return%201%0A%20%20%20%20elif%20total%20%3C%200%3A%0A%20%20%20%20%20%20%20%20return%200%0A%20%20%20%20elif%20cents%20%3E%20total%3A%0A%20%20%20%20%20%20%20%20return%200%0A%20%20%20%20else%3A%20%0A%20%20%20%20%20%20%20%20return%20count_change%28total%2C%20cents%20*%202%29%20%2B%20count_change%28total%20-%20cents%2C%20cents%29&function_call=count_change%287%2C%201%29)
 
 
+
+
+
+# Q4: Missing Digits
+def missing_digits(n):
+    if n < 10:
+        return 0
+    if ((n % 100) // 10) == ((n % 10)):
+        return missing_digits(n // 10)
+    if ((n % 100) // 10) != ((n % 10) - 1):
+            return (((n % 10) - 1) - ((n % 100) // 10)) + missing_digits(n // 10)
+    else:
+        return 0
