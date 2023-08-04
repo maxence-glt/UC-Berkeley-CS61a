91a1dd98c79213ac56291b6efa407097f0a85ed5
maxence-glt <mgilloteaux05@gmail.com>
2023-08-04 01:50:42 -0400

    Guelrilla 1 completed

--- Guerrilla 1.py
+++ Guerrilla 1.py
@@ -1,13 +1,16 @@
-#1.3
+# Guerilla 01 of UC Berkeley's cs61a spring 2020 course
+# https://inst.eecs.berkeley.edu/~cs61a/sp20/disc/guer01.pdf
+
+# 1.3
 def map_mut(f, L):
     for x in L: 
         L[x-1] = f(x)
     
-L = [1, 2, 3, 4]
-map_mut(lambda x: x**2, L)
-L #[1, 4, 9, 16]
 
-#1.10
+
+
+
+# 1.10
 def paths(m, n):
     if m == 1 or n == 1:
         return 1
@@ -27,4 +30,19 @@ def merge(s1, s2):
 
 #1.12
 def mario_number(level):
-    i
+    length = len(level)
+    def helper(level, length):
+        if length == 1 or length == 2:
+            return 1
+        if level[1] == "P" and level[2] == "P":
+            return False
+        if level[0] == "-" and level[1] == "-" and level[2] == "-":
+            return helper(level[1:], length - 1) + helper(level[2:], length - 2)
+        if level[1] == "-":
+            return helper(level[1:], length - 1)
+        if level[1] == "P":
+            return helper(level[2:], length - 2)
+    if helper(level, length) == 0:
+        return 0
+    else:
+        return helper(level, length)
