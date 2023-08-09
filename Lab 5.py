237685492b1c2cb7c8a160f57d4536308fc8befd
maxence-glt <mgilloteaux05@gmail.com>
2023-08-08 21:59:05 -0400

    Lab 5 completed

--- Lab 5.py
+++ Lab 5.py
@@ -82,11 +82,42 @@ def is_leaf(tree):
     return not branches(tree)
 
 def nut_finder(t):
-    for x in t:
-        if label(x) == "nut":
+    if label(t) == "nut":
+        return True
+    for x in branches(t):
+        if nut_finder(x) == True:
             return True
-        if is_leaf(t) == True:
-            return False
-        else:
-            return nut_finder(branches(t))
-    
+    return False
+
+
+
+
+
+# Q7: Sprout leaves
+def print_tree(t, indent=0):
+    print('  ' * indent + str(label(t)))
+    for b in branches(t):
+        print_tree(b, indent + 1)
+
+def sprout_leaves(t, values):
+    if is_leaf(t):
+        return tree(t, tree(values[0], [tree(values[1])]))
+    return tree(label(t), [sprout_leaves(b, values) for b in branches(t)])
+
+
+
+
+
+# Q8: Add Characters
+def add_chars(w1, w2):
+    if w1 == "":
+        return w2
+    if w2 == "":
+        return ""
+    else: 
+        for x in w1:
+            for y in w2:
+                if x != y:
+                    return y + add_chars(w1, w2[1:])
+                else:
+                    return add_chars(w1[1:], w2[1:])
