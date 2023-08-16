b74304c682c4b4e5e572b407eb0512f82d3cca00
maxence-glt <maxencegilloteaux2023@gmail.com>
2023-08-16 19:34:34 -0400

    Lab 6 completed

--- Labs/Lab 6.py
+++ Labs/Lab 6.py
@@ -10,16 +10,45 @@ def make_adder_inc(n):
         return n + x + inc
     return one_argument_function
 
-adder1 = make_adder_inc(5)
 
-adder2 = make_adder_inc(6)
 
-print(adder1(2))
 
-print(adder1(2)) # 5 + 2 + 1
 
-print(adder1(10)) # 5 + 10 + 2
+# Q2: Next Fibonacci
+# Fib seq =  0, 1, 1, 2, 3, 5, 8, 13, 21, 34
+def make_fib():
+    a, b = 0, 1
+    def next_fib():
+        nonlocal a, b
+        a, b = b, b + a
+        return a
+    return next_fib
 
-print([adder1(x) for x in [1, 2, 3]])
 
-print(adder2(5))
+
+
+
+# Q3: Scale
+def scale(it, multiplier):
+    i = 0
+    while i < len(it):
+        yield it[i] * 5
+        i += 1
+
+
+
+
+
+m = scale2([1, 5, 2], 5)
+
+print(list(m))
+
+# Q4: Hailstone
+def hailstone(n):
+    while n != 1:
+        yield n
+        if ((n % 2) == 0):
+            n = n // 2
+        else:
+            n = (n * 3) + 1
+    yield n
