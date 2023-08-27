fd09b411b479c36eb5496e2e8e5f3354b5c4ea27
maxence-glt <maxencegilloteaux2023@gmail.com>
2023-08-27 01:22:47 -0400

    2.1 finished

--- Discussions/Discussion 6.py
+++ Discussions/Discussion 6.py
@@ -17,3 +17,58 @@ f = memory(10)
 
 
 # 1.3
+# def nonlocalist():
+
+
+
+
+# prepend, get = nonlocalist()
+
+# prepend(2)
+
+# prepend(3)
+
+# prepend(4)
+
+# print(get(0))
+
+# print(get(1))
+
+# print(get(2))
+
+# prepend(8)
+
+# print(get(2))
+
+
+
+
+
+# 2.1
+def merge(a, b):
+    while True:
+        x, y = next(a), next(b)
+        if x == y:
+            yield x
+            x, y = next(a), next(b)
+        else:
+            yield max(x, y)
+            yield min(x, y)
+            x, y = next(a), next(b)
+
+
+
+
+def sequence(start, step):
+    while True:
+        yield start
+        start += step
+
+a = sequence(2, 3)
+
+b = sequence(3, 2)
+
+result = merge(a, b) 
+
+print([next(result) for _ in range(10)])
+
