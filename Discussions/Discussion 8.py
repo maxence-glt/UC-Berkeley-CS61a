76a3b63ffe06638c0df09af1810de5bff4d6ecfb
maxence-glt <maxencegilloteaux2023@gmail.com>
2023-09-07 14:05:49 -0400

    discussion 8 finished

---
+++ Discussions/Discussion 8.py
@@ -0,0 +1,22 @@
+# Discussion 8 of UC Berkeley's cs61a spring 2020 course
+# https://inst.eecs.berkeley.edu/~cs61a/sp20/disc/disc08.pdf
+
+# 1.1
+# The efficiency of bonk is logarithmic
+# The return and total are constant
+# The while loop only takes an extra step every 2n - it doesn't look at every item
+
+
+
+
+
+# 1.2
+# The efficiency of is_prime is linear
+# This is because it looks at every value up to n
+
+
+
+
+
+# 1.3 
+# The efficiency of mod_7 is linear
