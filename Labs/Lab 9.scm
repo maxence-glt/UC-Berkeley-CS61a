ecca4ce17775a75913a63260e1bab5d5ccecbbb6
maxence-glt <maxencegilloteaux2023@gmail.com>
2023-11-11 20:44:14 -0500

    lab 9 completed

--- Labs/Lab 9.scm
+++ Labs/Lab 9.scm
@@ -27,7 +27,78 @@
 
 ; Q4: Make Adder
 (define (make-adder n)
-  (lambda adder (n m) (+ n m))
-  adder
+  (define (make-adder-helper m) (+ n m))
+  make-adder-helper
 )
-  
+
+
+
+
+
+; Q5: Make a List
+(define lst
+  (cons (cons 1 nil)
+       (cons 2 (cons
+                (cons 3
+                      (cons 4 nil))
+                (cons 5 nil))))
+)
+
+
+
+
+
+; Q6: Compose
+(define (composed f g)
+  (define (output x) (f (g x)))
+  output
+)
+
+
+
+
+
+; Q7: Remove
+(define (remove item lst)
+  (cond ((eq? lst nil) nil)
+        ((eq? (car lst) item) (remove item (cdr lst)))
+        (else (cons (car lst) (remove item (cdr lst))))
+  )
+)
+
+
+
+
+
+; Q8: No Repeats
+(define (no-repeats s)
+  (if (eq? nil s)
+      s
+      (cons (car s) (no-repeats (filter-lst (lambda (x) (not (eq? x (car s)))) (cdr s))))
+  )
+)
+
+
+
+
+
+; Q9: Substitute
+(define (substitute s old new)
+  (cond ((eq? s nil) nil)
+        ((eq? (car s) old) (cons new (substitute (cdr s) old new)))
+        (else (cons (car s) (substitute (cdr s) old new)))
+  )
+)
+
+
+
+
+
+; Q10: Sub All
+(define (sub-all s olds news)
+  (cond ((null? s) nil)
+        ((null? olds) s)
+        ((eq? (car s) (car olds)) (cons (car news) (sub-all (cdr s) (cdr olds) (cdr news))))
+        (else (sub-all s (cdr olds) (cdr news)))
+  )
+)
