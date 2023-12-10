eda38f86e011340cd1b27046b66b01dfd01eb463
maxence-glt <maxencegilloteaux2023@gmail.com>
2023-12-10 17:39:18 -0500

    typo in linking

--- Homeworks/HW 06.scm
+++ Homeworks/HW 06.scm
@@ -1,5 +1,5 @@
-; HW04 of UC Berkeley's cs61a spring 2020 course
-; https://inst.eecs.berkeley.edu/~cs61a/sp20/hw/hw04/
+; HW06 of UC Berkeley's cs61a spring 2020 course
+; https://inst.eecs.berkeley.edu/~cs61a/sp20/hw/hw06/
 
 ; Q2: Thane of Cadr
 (define (cddr s)
